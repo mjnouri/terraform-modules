df1f77cc40fe7c31c72f37d654c26dab141540d5
Mark Nouri <mjnouri@gmail.com>
2022-04-04 14:13:11 -0400

    added key_name var

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "instance" {
   ami                    = var.ami
   instance_type          = var.instance_type
+  key_name               = var.key_name
   tags = {
     Name         = "${var.project_name}-${var.env}-ec2"
     Environment  = var.env
